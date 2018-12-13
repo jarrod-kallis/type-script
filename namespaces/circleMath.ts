e5e73c469e9e6be5073b1221d6835104332ac1c6
Jarrod Kallis <jarrod.kallis@gmail.com>
2018-12-13 04:11:34 +0200

    Nested namespaces

--- namespaces/circleMath.ts
+++ namespaces/circleMath.ts
@@ -1,5 +1,8 @@
 namespace MyMath {
-  const PI: number = 3.14;
+  export namespace Circle {
+    const PI: number = 3.14;
 
-  export const calcCircumference = (diameter: number): number => diameter * PI;
+    export const calcCircumference = (diameter: number): number =>
+      diameter * PI;
+  }
 }
--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,8 +1,11 @@
 /// <reference path="./circleMath.ts" />
 /// <reference path="./rectangleMath.ts" />
 
+import CircleMath = MyMath.Circle;
+
 const PI = 2.99;
 
-console.log(MyMath.calcCircumference(3));
+console.log(CircleMath.calcCircumference(10));
+console.log(MyMath.Circle.calcCircumference(3));
 console.log(MyMath.calcRectangle(10, 20));
 console.log(PI);
