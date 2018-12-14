d28b4d0c342767962a2a9278c21b653f79194569
Jarrod Kallis <jarrod.kallis@gmail.com>
2018-12-14 20:21:37 +0200

    Decorators - advanced

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -16,3 +16,20 @@ function mustLog(value: boolean): Function {
 
 @mustLog(true)
 class DecoratedCar {}
+
+// Advanced
+function printable(constructorFunc: Function): void {
+  constructorFunc.prototype.print = function() {
+    // console.log(constructorFunc.prototype);
+    console.log(this);
+  };
+}
+
+@printable
+class DecoratedPlant {
+  name: string = 'Green Plant';
+}
+const p: DecoratedPlant = new DecoratedPlant();
+// Have to cast as 'any', because TS doesn't know at
+// compile time that the class will have a print method
+(<any>p).print();
