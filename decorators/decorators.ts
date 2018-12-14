921d216e6f655374d367221ed4cc9df6860bea79
Jarrod Kallis <jarrod.kallis@gmail.com>
2018-12-14 21:40:39 +0200

    Decorators - method

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,3 +1,5 @@
+// http://www.typescriptlang.org/docs/handbook/decorators.html
+
 function logged(constructorFunc: Function) {
   console.log(constructorFunc);
 }
@@ -34,3 +36,48 @@ const p: DecoratedPlant = new DecoratedPlant();
 // Have to cast as 'any', because TS doesn't know at
 // compile time that the class will have a print method
 (<any>p).print();
+
+// Method
+function editable(value: boolean): Function {
+  return function(
+    target: any,
+    propName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log(target);
+    console.log(propName);
+    descriptor.writable = value;
+  };
+}
+
+class DecoratedProject {
+  private projectName: string;
+
+  constructor(name: string) {
+    this.projectName = name;
+  }
+
+  @editable(true)
+  calcBudget() {
+    console.log(this.projectName, 1000);
+  }
+
+  @editable(false)
+  static calcSomething() {
+    console.log('Something');
+  }
+}
+
+const project: DecoratedProject = new DecoratedProject('Super Project');
+project.calcBudget();
+project.calcBudget = function() {
+  console.log(2000);
+};
+project.calcBudget();
+
+DecoratedProject.calcSomething();
+// Results in an error, because method can't be changed
+// DecoratedProject.calcSomething = function() {
+//   console.log('Something else');
+// };
+DecoratedProject.calcSomething();
